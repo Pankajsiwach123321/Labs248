7086f5e401b4181d0e9d94c2ae3b055e06265180
Pankajsiwach123321 <pankusiwach000@gmai.com>
2024-03-18 10:23:06 +0530

    footer logo cursurepointer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,9 @@ const Footer = () => {
   return (
     <footer className=" bg-black position-relative z-1 overflow- hidden -mt-1 overflow-xclip">
       <div className=" container text-center mb-3 ">
-        <img src={logo} alt="logo" width={287} />
+        <a href="#">
+          <img src={logo} alt="logo" width={287} />
+        </a>
         <div className=" d-flex align-items-center gap-4 justify-content-center mt-3  pt-1 ">
           <a
             href="#Home"
@@ -36,16 +38,32 @@ const Footer = () => {
           </a>
         </div>
         <div className=" d-flex gap-3 align-items-center justify-content-center pt-4">
-          <a href="https://twitter.com/account/access" target="_blanck" className="svg-up">
+          <span
+            href="https://twitter.com/account/access"
+            target="_blanck"
+            className="svg-up"
+          >
             <Twitter />
-          </a>
-          <a href="https://www.linkedin.com/" target="_blanck" className="svg-up">
+          </span>
+          <a
+            href="https://www.linkedin.com/"
+            target="_blanck"
+            className="svg-up"
+          >
             <Facebook />
           </a>
-          <a href="https://www.linkedin.com/" target="_blanck" className="svg-up">
+          <a
+            href="https://www.linkedin.com/"
+            target="_blanck"
+            className="svg-up"
+          >
             <Instagram />
           </a>
-          <a href="https://www.linkedin.com/" target="_blanck" className="svg-up">
+          <a
+            href="https://www.linkedin.com/"
+            target="_blanck"
+            className="svg-up"
+          >
             <Linkin />
           </a>
         </div>
